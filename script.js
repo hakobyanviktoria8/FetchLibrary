410be80f4a1d67a1de2f0dab8cdf24bd4884f71e
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-06-10 18:06:46 +0400

    JS ACA

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ searchBtn.onclick = async () => {
     let url = 'https://www.googleapis.com/books/v1/volumes?q='+bookName;
     let response = await fetch(url);
     let commits = await response.json();
-    console.log(commits);
     for (let book of commits.items){
         let avatar = book.volumeInfo.imageLinks.thumbnail;
         let page = book.volumeInfo.previewLink;
@@ -14,7 +13,7 @@ searchBtn.onclick = async () => {
 
         let card =
             `<div class="col-lg-2 overflow-auto my-4 border">
-                    <a href="${page}"><img src = ${avatar} class="w-100" title="See profile"></a>
+                    <a href="${page}"><img src = ${avatar} class="w-100" title="See book"></a>
                     <h4 class="text-center">${title}</h4>
                     <h6>Authors ${authors}</h6>
                     <h6>Published date ${publishedDate}</h6>
